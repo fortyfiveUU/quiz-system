40e5f390d19bc4d69ed6a6df506686e0146308c1
William <hamilton-w6@ulster.ac.uk>
2026-01-02 21:28:43 +0000

    Add files via upload

---
+++ QuizSystem.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace QuizSystem
+{
+
+    public class QuizSystem
+    {
+        public void Run()
+        {
+            
+
+            Console.WriteLine("Online Quiz System starting");
+        }
+    }
+}
+
+
